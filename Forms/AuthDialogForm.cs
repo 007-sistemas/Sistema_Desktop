a8eda27d886c93545a905806206d839a9f24f328
Gabriel Gomes <gabrielgomes@coopen.com.br>
2026-02-02 13:28:04 -0300

    Implementar melhorias na autenticação administrativa (timeout 1min, erro sem botões com auto-close 3seg)

--- Forms/AuthDialogForm.cs
+++ Forms/AuthDialogForm.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BiometricSystem.Forms
@@ -7,9 +8,12 @@ namespace BiometricSystem.Forms
     {
         public string Password => txtPassword.Text;
         public bool AuthSuccess { get; private set; } = false;
-        private readonly Func<string, System.Threading.Tasks.Task<bool>> _authCallback;
+        private readonly Func<string, Task<bool>> _authCallback;
+        private System.Threading.Timer _inactivityTimer;
+        private const int INACTIVITY_TIMEOUT_MS = 60000; // 60 segundos (1 minuto)
+        private const int ERROR_TIMEOUT_MS = 3000; // 3 segundos para erro
 
-        public AuthDialogForm(Func<string, System.Threading.Tasks.Task<bool>> authCallback)
+        public AuthDialogForm(Func<string, Task<bool>> authCallback)
         {
             InitializeComponent();
             this.TopMost = true;
@@ -20,6 +24,7 @@ namespace BiometricSystem.Forms
                 this.Activate();
                 this.Focus();
                 this.txtPassword.Focus();
+                IniciarTimerInatividade();
             };
             this.Activated += (s, e) => {
                 this.BringToFront();
@@ -29,30 +34,31 @@ namespace BiometricSystem.Forms
             };
 
             // Eventos do teclado virtual
-            this.btnNum0.Click += (s, e) => txtPassword.Text += "0";
-            this.btnNum1.Click += (s, e) => txtPassword.Text += "1";
-            this.btnNum2.Click += (s, e) => txtPassword.Text += "2";
-            this.btnNum3.Click += (s, e) => txtPassword.Text += "3";
-            this.btnNum4.Click += (s, e) => txtPassword.Text += "4";
-            this.btnNum5.Click += (s, e) => txtPassword.Text += "5";
-            this.btnNum6.Click += (s, e) => txtPassword.Text += "6";
-            this.btnNum7.Click += (s, e) => txtPassword.Text += "7";
-            this.btnNum8.Click += (s, e) => txtPassword.Text += "8";
-            this.btnNum9.Click += (s, e) => txtPassword.Text += "9";
+            this.btnNum0.Click += (s, e) => { txtPassword.Text += "0"; ReiniciarTimerInatividade(); };
+            this.btnNum1.Click += (s, e) => { txtPassword.Text += "1"; ReiniciarTimerInatividade(); };
+            this.btnNum2.Click += (s, e) => { txtPassword.Text += "2"; ReiniciarTimerInatividade(); };
+            this.btnNum3.Click += (s, e) => { txtPassword.Text += "3"; ReiniciarTimerInatividade(); };
+            this.btnNum4.Click += (s, e) => { txtPassword.Text += "4"; ReiniciarTimerInatividade(); };
+            this.btnNum5.Click += (s, e) => { txtPassword.Text += "5"; ReiniciarTimerInatividade(); };
+            this.btnNum6.Click += (s, e) => { txtPassword.Text += "6"; ReiniciarTimerInatividade(); };
+            this.btnNum7.Click += (s, e) => { txtPassword.Text += "7"; ReiniciarTimerInatividade(); };
+            this.btnNum8.Click += (s, e) => { txtPassword.Text += "8"; ReiniciarTimerInatividade(); };
+            this.btnNum9.Click += (s, e) => { txtPassword.Text += "9"; ReiniciarTimerInatividade(); };
 
-            // btnLimpar removido
-                this.btnLimparPrincipal.Click += (s, e) => txtPassword.Text = string.Empty;
-
+            // Botão Limpar
+            this.btnLimparPrincipal.Click += (s, e) => { txtPassword.Text = string.Empty; ReiniciarTimerInatividade(); };
 
             // Botão Cancelar fecha o modal
             this.btnCancel.Click += (s, e) =>
             {
+                PararTimerInatividade();
                 this.DialogResult = DialogResult.Cancel;
                 this.Close();
             };
 
             this.btnOk.Click += async (s, e) =>
             {
+                PararTimerInatividade();
                 this.Enabled = false;
                 try
                 {
@@ -68,22 +74,25 @@ namespace BiometricSystem.Forms
                         txtPassword.Text = string.Empty;
                         this.DialogResult = DialogResult.Cancel;
                         this.Enabled = true;
+                        IniciarTimerInatividade();
                         return;
                     }
                     if (result)
                     {
+                        // Senha correta: fechar sem popup, apenas destrava a tela
                         AuthSuccess = true;
-                        MessageBox.Show("Acesso liberado!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         this.DialogResult = DialogResult.OK;
                         this.Close();
                     }
                     else
                     {
+                        // Senha errada: mostrar "Senha Inválida" sem botões e fechar em 3 segundos
                         AuthSuccess = false;
-                        MessageBox.Show("Acesso não autorizado!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         txtPassword.Text = string.Empty;
                         this.DialogResult = DialogResult.Cancel;
-                        this.Enabled = true;
+                        
+                        // Mostrar mensagem de erro sem botão OK
+                        ExibirErroSemBotao();
                     }
                 }
                 finally
@@ -91,6 +100,101 @@ namespace BiometricSystem.Forms
                     this.Enabled = true;
                 }
             };
+
+            this.FormClosing += (s, e) =>
+            {
+                PararTimerInatividade();
+            };
+        }
+
+        private void IniciarTimerInatividade()
+        {
+            PararTimerInatividade();
+            _inactivityTimer = new System.Threading.Timer((state) =>
+            {
+                if (InvokeRequired)
+                {
+                    Invoke(() => FecharPorInatividade());
+                }
+                else
+                {
+                    FecharPorInatividade();
+                }
+            }, null, INACTIVITY_TIMEOUT_MS, System.Threading.Timeout.Infinite);
+        }
+
+        private void ReiniciarTimerInatividade()
+        {
+            IniciarTimerInatividade();
+        }
+
+        private void PararTimerInatividade()
+        {
+            if (_inactivityTimer != null)
+            {
+                _inactivityTimer.Dispose();
+                _inactivityTimer = null;
+            }
+        }
+
+        private void FecharPorInatividade()
+        {
+            if (this.Visible)
+            {
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            }
+        }
+
+        private void ExibirErroSemBotao()
+        {
+            // Criar form de erro customizado sem botões
+            Form erroForm = new Form();
+            erroForm.Text = "Erro";
+            erroForm.Size = new System.Drawing.Size(300, 150);
+            erroForm.StartPosition = FormStartPosition.CenterParent;
+            erroForm.TopMost = true;
+            erroForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+            erroForm.MaximizeBox = false;
+            erroForm.MinimizeBox = false;
+            erroForm.ControlBox = false;
+            erroForm.BackColor = System.Drawing.Color.FromArgb(240, 240, 240);
+
+            // Label com mensagem
+            Label lblMensagem = new Label();
+            lblMensagem.Text = "Senha Inválida";
+            lblMensagem.Font = new System.Drawing.Font("Arial", 14, System.Drawing.FontStyle.Bold);
+            lblMensagem.ForeColor = System.Drawing.Color.FromArgb(200, 0, 0);
+            lblMensagem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lblMensagem.Dock = DockStyle.Fill;
+            erroForm.Controls.Add(lblMensagem);
+
+            // Timer para fechar após 3 segundos
+            System.Threading.Timer timer = null;
+            timer = new System.Threading.Timer((state) =>
+            {
+                if (erroForm.InvokeRequired)
+                {
+                    erroForm.Invoke(() =>
+                    {
+                        timer?.Dispose();
+                        erroForm.Close();
+                    });
+                }
+                else
+                {
+                    timer?.Dispose();
+                    erroForm.Close();
+                }
+            }, null, ERROR_TIMEOUT_MS, System.Threading.Timeout.Infinite);
+
+            erroForm.ShowDialog(this);
+            
+            // Após fechar o erro, fechar o dialog de autenticação
+            if (this.Visible)
+            {
+                this.Close();
+            }
         }
     }
 }
--- obj/Release/net8.0-windows/win-x64/BiometricSystem.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/BiometricSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiometricSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ce989bb4624282ccdaa890c9e5839460ff7781")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiometricSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiometricSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
